49b4a13d84e8af65e6bbb15d16a01301e050fca3
Yosshi72 <faily.nappy.yshk@gmail.com>
2024-03-04 17:18:29 +0900

    Add: vscode-keybindings
    - cmd+': create terminal
    - cmd+k cmd+{j,k,h,l}: split editor

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-
+# for Mac User
 CWD=$HOME/Dotfiles
+VSCODE=$HOME/Library/Application\ Support/Code/User
 
 #vim
 echo 'Create the symbolic link for vim'
@@ -12,7 +13,18 @@ echo 'Create the symbolic link for neovim'
 ln -sf $CWD/nvim/init.vim $HOME/.config/nvim/init.vim
 ln -sf $CWD/nvim/dein.toml $HOME/.config/dein/dein.toml
 ln -sf $CWD/nvim/dein_lazy.toml $HOME/.config/dein/dein_lazy.toml
+ln -sf $CWD/nvim/dein_lang.toml $HOME/.config/dein/dein_lang.toml
 
 #zsh
 echo 'Create the symbolic link for zsh'
 ln -sf $CWD/.zshrc $HOME/.zshrc
+
+#wezterm
+echo 'Create the symbolic link for wezterm'
+ln -sf $CWD/wezterm.lua $HOME/.config/wezterm/wezterm.lua
+
+#vscode
+echo 'Create the symbolic link for vscode settings'
+ln -sf $CWD/Vscode/keybindings.json $VSCODE/keybindings.json
+ln -sf $CWD/Vscode/settings.json $VSCODE/settings.json
+
